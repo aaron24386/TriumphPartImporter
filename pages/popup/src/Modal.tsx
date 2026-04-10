f70afd2124dbaf4772d11b74d386b9560006128c
Aaron Paquette <aaronpaquette3@gmail.com>
2026-04-09 19:09:07 -0600

    adding keyboard in modal navigation

--- pages/popup/src/Modal.tsx
+++ pages/popup/src/Modal.tsx
@@ -14,8 +14,26 @@ const Modal = ({
 }) => {
   return (
     <div className={`flex py-1`}>
-      <div className="modal-overlay" onClick={() => setShowModal(false)}>
-        <div className="modal-content" onClick={e => e.stopPropagation()}>
+      <div
+        className="modal-overlay"
+        onClick={() => setShowModal(false)}
+        role="button"
+        tabIndex={0}
+        onKeyDown={e => {
+          if (e.key === 'Enter') {
+            setShowModal(false);
+          }
+        }}>
+        <div
+          className="modal-content"
+          onClick={e => e.stopPropagation()}
+          role="button"
+          tabIndex={0}
+          onKeyDown={e => {
+            if (e.key === 'Enter') {
+              e.stopPropagation();
+            }
+          }}>
           <p className="text-gray-800 text-base mb-6">
             Are you sure you want to delete <b>{basketName}</b>?
           </p>
@@ -23,11 +41,13 @@ const Modal = ({
             <button
               className="px-4 py-2 rounded bg-gray-200 hover:bg-gray-300 text-gray-800"
               onClick={() => setShowModal(false)}>
+              tabIndex={0}
               Cancel
             </button>
             <button
               className="px-4 py-2 rounded bg-red-500 hover:bg-red-600 text-white"
               onClick={() => deleteBasket(basketId)}>
+              tabIndex={0}
               Confirm
             </button>
           </div>
